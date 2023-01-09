f1882b54a08272f3f2d3c3fe84366d6e0773887b
Andy Zhang <122180239+AndyZhangI@users.noreply.github.com>
2023-01-09 23:06:56 +0000

    Improved how the text and pictures are seen other than leaving the words together and hard to read.

--- main.py
+++ main.py
@@ -1,92 +1,114 @@
 # Cost of Each Product --------------------------------------------------
 Cost1 = [
-'---------------               ',
+'------------------------------',
 'Grocery:                      ',
-'---------------               ',
+'------------------------------',
 'Milk = 6$                     ',
 'Eggs = 14$                    ',
 'Bread = 10$                   ',
 'Cereal = 8$                   ',
+'                              ',
 ]
 
 Cost2 = [
-'---------------               ',
+'------------------------------',
 'Clothing:                     ',
-'---------------               ',
+'------------------------------',
 'Hat = 16$                     ',
 'Cap = 15$                     ',
 'Shirt = 18$                   ',
 'Pants = 20$                   ',
 'Socks = 17$                   ',
 'Shoes = 23$                   ',
+'                              ',
 ]
 
 Cost3 = [
-'----------------              ',
+'------------------------------',
 'Electronics:                  ',
-'----------------              ',
+'------------------------------',
 'Big Television = 400$         ',
 'Medium Television = 300$      ',
 'Small Television = 250$       ',
 'Tablet = 450$                 ',
 'Phone = 350$                  ',
+'                              ',
 ]
 
 # List of Products ------------------------------------------------------
 List = [
-'---------------               ',
-'Grocery:                      ',
-'---------------               ',
-'Milk                          ',
-'Eggs                          ',
-'Bread                         ',
-'Cereal                        ',
-'---------------               ',
-'Clothing:                     ',
-'---------------               ',
-'Hat                           ',
-'Cap                           ',
-'Shirt                         ',
-'Pants                         ',
-'Socks                         ',
-'Shoes                         ',
-'----------------              ',
-'Electronics:                  ',
-'----------------              ',
-'Big Television                ',
-'Medium Television             ',
-'Small Television              ',
-'Tablet                        ',
-'Phone                         ',
-
+'                              ',
+'          PRODUCTS            ',
+'------------------------------',
+'          Grocery:            ',
+'------------------------------',
+'         - Milk               ',
+'         - Eggs               ',
+'         - Bread              ',
+'         - Cereal             ',
+'------------------------------',
+'          Clothing:           ',
+'------------------------------',
+'         - Hat                ',
+'         - Cap                ',
+'         - Shirt              ',
+'         - Pants              ',
+'         - Socks              ',
+'         - Shoes              ',
+'------------------------------',
+'         Electronics:         ',
+'------------------------------',
+'      - Big Television        ',
+'      - Medium Television     ',
+'      - Small Television      ',
+'         - Tablet             ',
+'         - Phone              ',
+'                              ',
 ]
 
 # Mapping ---------------------------------------------------------------
 map = [
-'___________________           ',
-'|#####.&&&&&.%%%%%|           ',
-'|#####.&&&&&.%%%%%|           ',
-'|#####.&&&&&.%%%%%|           ',
-'|.................|           ',
-'|..0..0..0..0..0..|           ',
-'-------------------           ',
-'Map legend:                   ',
-'0 = represents Cashier        ',
-'# = represents Grocery        ',
-'& = represents Clothing       ',
-'% = represents Electronics    ',
+'                              ',
+'      MAP OF THE STORE        ',
+'     ___________________      ',
+'     |#####.&&&&&.%%%%%|      ',
+'     |#####.&&&&&.%%%%%|      ',
+'     |#####.&&&&&.%%%%%|      ',
+'     |.................|      ',
+'     |..0..0..0..0..0..|      ',
+'     -------------------      ',
+'         Map legend:          ',
+'                              ',
+'   0 = represents Cashier     ',
+'                              ',
+'   # = represents Grocery     ',
+'                              ',
+'   & = represents Clothing    ',
+'                              ',
+'   % = represents Electronics ',
+'                              ',
 ]
 
 # Days and Hours---------------------------------------------------------
 import random #Imported from Library
 schedule = [
+'                              ',
+'        STORE HOURS           ',
+'                              ',
 'Monday: Open for 7 Hours      ',
+'                              ',
 'Tuesday: Open for 8 Hours     ',
+'                              ',
 'Wednesday: Open for 9 Hours   ',
+'                              ',
 'Thursday: Open for 7 Hours    ',
+'                              ',
 'Friday: Open for 10 Hours     ',
+'                              ',
 'Saturday: Open for 7 Hours    ',
+'                              ',
 'Sunday: Closed                ',
+'                              ',
 ]
 number = ['6','7','8','9','10']
 today = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
@@ -111,13 +133,13 @@ while True:
          choice1 = input('Would you like to know the store hours for "today" or "specific day?"\n')
          if choice1 == 'today':
            print(f'Today is {random.choice(today)} and the store would be open for {random.choice(number)} Hours.\n')
-           bot10 = input('Need anything else? Wave your hand!')
+           bot10 = input('Need anything else? Wave your hand!\n')
            if bot10 == 'Wave':
              bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
          if choice1 == 'specific day':
-           print('Here is the list of Store Hours. You are welcome to come back!')
+           print('Here is the list of Store Hours. You are welcome to come back!\n')
            print(schedule)
-           bot10 = input('Need anything else? Wave your hand!')
+           bot10 = input('Need anything else? Wave your hand!\n')
            if bot10 == 'Wave':
               bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
 # -------------------------- MAP -------------------------------------------
@@ -129,20 +151,20 @@ while True:
            direction = input('Where do you want to go? (Grocery, Clothes, Electronics)\n')
       
            if direction == 'Grocery':
-             print('Head to the left of the store.')
-             bot11 = input('Need anything else? Wave your hand!')
+             print('Head to the left of the store.\n')
+             bot11 = input('Need anything else? Wave your hand!\n')
              if bot11 == 'Wave':
                 bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
         
            elif direction == 'Clothes':
-             print ('Head to the center of the store.')
-             bot12 = input('Need anything else? Wave your hand!')
+             print ('Head to the center of the store.\n')
+             bot12 = input('Need anything else? Wave your hand!\n')
              if bot12 == 'Wave':
                 bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
            
            elif direction == 'Electronics':
-              print ('Head to the Right of the store.')
-              bot13 = input('Need anything else? Wave your hand!')
+              print ('Head to the Right of the store.\n')
+              bot13 = input('Need anything else? Wave your hand!\n')
               if bot13 == 'Wave':
                  bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
      
@@ -171,8 +193,8 @@ while True:
                product3 = input('Do you want to see the cost of other products? (yes/no)\n')
         
                while product3 == 'no':
-                  print('Okay, Wave your hand if you need anything else.')
-                  bot = input('---Ready with a question? Wave your hand!')
+                  print('Okay, Wave your hand if you need anything else.\n')
+                  bot = input('---Ready with a question? Wave your hand!\n')
           
                   if bot == 'Wave':
                      bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
@@ -185,8 +207,8 @@ while True:
                product3 = input('Do you want to see the cost of other products? (yes/no)\n')
         
                while product3 == 'no':
-                  print('Okay, Wave your hand if you need anything else.')
-                  bot = input('---Ready with a question? Wave your hand!')
+                  print('Okay, Wave your hand if you need anything else.\n')
+                  bot = input('---Ready with a question? Wave your hand!\n')
           
                   if bot == 'Wave':
                      bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
@@ -199,8 +221,8 @@ while True:
                product3 = input('Do you want to see the cost of other products? (yes/no)\n')
         
                while product3 == 'no':
-                  print('Okay, Wave your hand if you need anything else.')
-                  bot = input('---Ready with a question? Wave your hand!')
+                  print('Okay, Wave your hand if you need anything else.\n')
+                  bot = input('---Ready with a question? Wave your hand!\n')
           
                   if bot == 'Wave':
                      bot = input('Welcome to Targots! Do you have questions for the follow: Store hours, mapping of categories, list of products? (yes/no)\n')
